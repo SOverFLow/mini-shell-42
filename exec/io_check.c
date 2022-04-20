6697b0f77ce57411c466631884ad4a46efcb87b2
Salah eddine Elhandaoui <selhanda@e1r4p3.1337.ma>
2022-04-20 17:29:58 +0000

    hhh

--- exec/io_check.c
+++ exec/io_check.c
@@ -1,22 +1,59 @@
 #include "../minishell.h"
 
+int outfile_len(t_comp *head)
+{
+    int len;
+
+    len = 0;
+    while (head)
+    {
+        if (head->whatisthis == 4)
+            len++;
+        head = head->next;
+    }
+    return (len);
+}
+
+char *opens_files(char **files, int len)
+{
+    int i;
+    int fd;
+
+    i = 0;
+    while (i < len)
+    {
+        if (files[i + 1] == NULL)
+            return (files[i]);
+        else 
+        {
+            fd = open(files[i], O_WRONLY | O_CREAT,  0666);
+            close(fd);
+        }
+        i++;
+    }
+}
+
 char *is_outfile(t_comp *head)
 {
-    char *last;
-    int out;
+    char **files;
+    int i;
+    int len;
 
-    if (!head)
+    len = outfile_len(head);
+    files = malloc(sizeof(char *) * (len + 1));
+    i = 0;
+    if (!head || !files)
         return (NULL);
     while (head != NULL)
     {
         if (head->whatisthis == 4)
         {
-            out = open(head->data, O_WRONLY | O_CREAT, 0666);
-            last = head->data;
+            files[i] = head->data;
+            i++;
         }
         head = head->next;
     }
-    return (last);
+    return (opens_files(files, len));
 }
 
 char *is_infile(t_comp *head)
