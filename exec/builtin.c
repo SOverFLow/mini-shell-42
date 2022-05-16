589e27c4947f2a1517a3a44e8507d64f0ace4161
Salah eddine Elhandaoui <selhanda@e1r4p3.1337.ma>
2022-05-16 16:18:28 +0100

    edit

--- exec/builtin.c
+++ exec/builtin.c
@@ -96,8 +96,8 @@ int		execute_builtin_cmds(t_comp *comp, int infile, t_env *head)
 		ft_exit(comp);
 	if (ft_strncmp(comp->data, "env", 4) == 0)
 		 ft_env(head, comp, outfile);
-	// if (ft_strncmp(comp->data, "export", 7) == 0)
-	// 	ft_export()
+	if (ft_strncmp(comp->data, "export", 7) == 0)
+		ft_export(comp, head);
 	if (ft_strncmp(comp->data, "unset", 6) == 0)
 		ft_unset(comp, head);
 	return (fd[0]);
