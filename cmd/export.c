d42eb77b61a8da2b871ff5857a3d7ecc14b87eaa
Salah eddine Elhandaoui <selhanda@e1r4p3.1337.ma>
2022-05-27 12:04:55 +0100

    export

--- cmd/export.c
+++ cmd/export.c
@@ -12,6 +12,33 @@
 
 #include "../minishell.h"
 
+int ft_is_in_env(t_env *head, char *key)
+{
+	while (head)
+	{
+		if (ft_strncmp(head->key, key, ft_strlen(key)) == 0)
+			return (1);
+		head = head->next;
+	}
+	return (0);
+}
+
+void	edit_env_value_by_key(t_env **head, char *key, char *val)
+{
+	t_env *my_head;
+
+	my_head = *head;
+	while (my_head)
+	{
+		if (ft_strncmp(my_head->key, key, ft_strlen(key)) == 0)
+		{
+			my_head->val = ft_strdup(val);
+			return ;
+		}
+		my_head = my_head->next;
+	}
+}
+
 void	add_var(char *var, t_env *env_node)
 {
 	t_env	*tmp_node;
@@ -22,6 +49,11 @@ void	add_var(char *var, t_env *env_node)
 		split = ft_split(var, '=');
 	if (split[0] != NULL)
 	{
+		if (ft_is_in_env(env_node, split[0]))
+		{
+			edit_env_value_by_key(&env_node, split[0], split[1]);
+			return ;
+		}
 		if (env_node && env_node->key == NULL)
 		{
 			env_node->key = ft_strdup(split[0]);
@@ -50,41 +82,34 @@ void	add_var(char *var, t_env *env_node)
 
 void	ft_export(t_comp *comp, t_env *head, int outfile)
 {
-	char	**split;
 	char	*key;
+	int		err;
 
 	if (num_of_args(comp) == 1)
-		hidden_env(head, outfile);
-	if (num_of_args(comp) > 1)
 	{
-		if (comp->next->next)
-		{
-			key = comp->next->next->data;
-			if (key[0] == '=')
-			{
-				ft_print_error(&key[0]);
-				g_status = 1;
-				return ;
-			}
-		}
+		hidden_env(head, outfile);
+		return ;
+	}
+	else
+	{
 		while (comp->next)
 		{
+			err = check_var_env(comp->next->data);
+			if (err == 0)
+			{
+				ft_print_error(comp->next->data);
+				g_status = 1;
+			}
 			key = comp->next->data;
 			if (key[0] == '=')
 			{
 				ft_print_error(&key[0]);
 				g_status = 1;
-				return ;
 			}
-			if (check_var_env(comp->next->data))
+			if (err == 1)
 			{
 				if (comp->next->data != NULL)
 					add_var(comp->next->data, head);
-			}
-			else
-			{
-				ft_print_error(comp->next->data);
-				g_status = 1;
 			}
 			comp = comp->next;
 		}
