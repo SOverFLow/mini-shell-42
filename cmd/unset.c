e394206a02ab8574e9a83b79258badc6bc6f8711
Salah eddine Elhandaoui <selhanda@e1r4p3.1337.ma>
2022-05-27 14:27:16 +0100

    edit

--- cmd/unset.c
+++ cmd/unset.c
@@ -12,57 +12,51 @@
 
 #include "../minishell.h"
 
-void	free_env(t_env *env_node)
+void	free_env(t_env *env)
 {
-	free(env_node->key);
-	free(env_node->val);
-	free(env_node);
+	free(env->key);
+	free(env->val);
+	free(env);
 }
 
-void	ft_unset(t_comp *comp, t_env **env_node)
+void	ft_delete_env(t_env **env, char *key)
 {
-	t_env	*tmp_node;
-	t_env   *node;
+	t_env	*node;
+	t_env	*tmp_env;
 
-	node = *env_node;
+	node = *env;
+	if (ft_strncmp(node->key, key, ft_strlen(key)) == 0)
+	{
+		*env = node->next;
+		free_env(node);
+		return ;
+	}
+	tmp_env = node;
+	node = node->next;
+	while (node)
+	{
+		if (ft_strncmp(node->key, key, ft_strlen(key)) == 0)
+		{
+			tmp_env->next = node->next;
+			free_env(node);
+			return ;
+		}
+		tmp_env = node;
+		node = node->next;
+	}
+}
+
+void	ft_unset(t_comp *comp, t_env **env)
+{
 	if (num_of_args(comp) == 1)
 	{
 		g_status = 1;
-		ft_putstr_fd("unset: not enough arguments\n", 2);
+		ft_putstr_fd("unset: not enough arguments", 2);
 		return ;
 	}
-	if (num_of_args(comp) > 1)
+	while (comp->next)
 	{
-		// if (ft_strncmp(comp->next->data, node->key,
-		// 		ft_strlen(node->key) + 1) == 0)
-		// {
-		// 	*env_node = node->next;
-		// 	free_env(node);
-		// 	return ;
-		// }
-		tmp_node = node;
-		node = node->next;
-		while (node && node->next)
-		{
-			if (ft_strncmp(comp->next->data, node->key,
-					ft_strlen(node->next->key)) == 0)
-			{
-				tmp_node->next = node->next;
-				free_env(node);
-				return ;
-			}
-			tmp_node = node;
-			node = node->next;
-		}
-		if (node->next == NULL)
-		{
-			if (ft_strncmp(comp->next->data, node->key,
-					ft_strlen(node->key) + 1) == 0)
-			{
-				tmp_node->next = NULL;
-				free_env(node);
-				return ;
-			}
-		}
+		ft_delete_env(env, comp->next->data);
+		comp = comp->next;
 	}
 }
